4cb8a16896af62e47ddc656c90bb5ea87e145e54
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:52:27 +0530

    Create 2999-CheckIfStringsCanBeMadeEqualWithOperationsI

---
+++ 2999-CheckIfStringsCanBeMadeEqualWithOperationsI/2999-CheckIfStringsCanBeMadeEqualWithOperationsI.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/3/2026, 1:52:27 PM
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        if (s1[0] > s1[2])
+            swap(s1[0], s1[2]);
+        if (s1[1] > s1[3])
+            swap(s1[1], s1[3]);
+        if (s2[0] > s2[2])
+            swap(s2[0], s2[2]);
+        if (s2[1] > s2[3])
+            swap(s2[1], s2[3]);
+
+        return s1 == s2;
+    }
+};
