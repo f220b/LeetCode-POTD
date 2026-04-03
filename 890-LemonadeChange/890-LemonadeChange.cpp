b1f73d179b8ac4d978d99ce167c49821a9a4d322
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:58:37 +0530

    Create 890-LemonadeChange

---
+++ 890-LemonadeChange/890-LemonadeChange.cpp
@@ -0,0 +1,27 @@
+// Last updated: 4/3/2026, 1:58:37 PM
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int fives = 0, tens = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if (fives == 0)
+                    return false;
+                fives--;
+                tens++;
+            } else {
+                if (fives > 0 && tens > 0) {
+                    fives--;
+                    tens--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
