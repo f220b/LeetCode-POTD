a24f0a4354e9649349e31f9fc7c7bca83ff1fc96
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 14:03:17 +0530

    Create 72-EditDistance

---
+++ 72-EditDistance/72-EditDistance.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/3/2026, 2:03:16 PM
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length(), n = word2.length();
+        vector<int> prev(n + 1, 0);
+
+        for (int j = 0; j <= n; j++) 
+            prev[j] = j;
+        
+        for (int i = 1; i <= m; i++) {
+            vector<int> curr(n + 1, 0);
+            curr[0] = i;
+            for (int j = 1; j <= n; j++) {
+                if (word1[i - 1] == word2[j - 1])
+                    curr[j] = prev[j - 1];
+                else
+                    curr[j] = 1 + min({prev[j - 1], prev[j], curr[j - 1]});
+            }
+            prev = curr;
+        }
+        return prev[n];
+    }
+};
