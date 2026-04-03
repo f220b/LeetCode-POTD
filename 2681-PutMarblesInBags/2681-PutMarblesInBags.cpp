5fe1c367d763a4238dee91af25ae3460b65d6398
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:52:56 +0530

    Create 2681-PutMarblesInBags

---
+++ 2681-PutMarblesInBags/2681-PutMarblesInBags.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/3/2026, 1:52:55 PM
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+        vector<long long> pairSums;
+        for (int i = 0; i < n - 1; i++) {
+            pairSums.push_back((long long)weights[i] + weights[i + 1]);
+        }
+        sort(pairSums.begin(), pairSums.end());
+        long long minSum = 0, maxSum = 0;
+        for (int i = 0; i < k - 1; i++) {
+            minSum += pairSums[i];
+            maxSum += pairSums[n - 2 - i];
+        }
+        return maxSum - minSum;
+    }
+};
