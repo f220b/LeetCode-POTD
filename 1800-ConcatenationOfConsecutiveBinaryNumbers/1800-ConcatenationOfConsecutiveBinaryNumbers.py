d1b6542b5e6aa4def7c0f4d9c5dbebf7e35cadab
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:55:51 +0530

    Create 1800-ConcatenationOfConsecutiveBinaryNumbers

---
+++ 1800-ConcatenationOfConsecutiveBinaryNumbers/1800-ConcatenationOfConsecutiveBinaryNumbers.py
@@ -0,0 +1,14 @@
+# Last updated: 4/3/2026, 1:55:51 PM
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        MOD = 10**9 + 7
+        res = 0
+        length = 0
+        
+        for i in range(1, n + 1):
+            if i & (i - 1) == 0:
+                length += 1
+            
+            res = ((res << length) | i) % MOD
+            
+        return res
