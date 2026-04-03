5423e0d6cf00024c68a44fd0c45e94aa9ceb8f5c
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:54:42 +0530

    Create 2144-MaximumDifferenceBetweenIncreasingElements

---
+++ 2144-MaximumDifferenceBetweenIncreasingElements/2144-MaximumDifferenceBetweenIncreasingElements.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/3/2026, 1:54:42 PM
+class Solution {
+public:
+    int maximumDifference(vector<int>& nums) {
+        int minVal = nums[0];
+        int maxDiff = -1;
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] > minVal) {
+                maxDiff = max(maxDiff, nums[i] - minVal);
+            } else {
+                minVal = nums[i];
+            }
+        }
+        return maxDiff;
+    }
+};
