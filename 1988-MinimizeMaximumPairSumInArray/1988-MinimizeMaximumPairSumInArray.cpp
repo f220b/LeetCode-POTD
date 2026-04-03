06146bb9992cdad8b421d377ba5ec8498590a475
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:55:18 +0530

    Create 1988-MinimizeMaximumPairSumInArray

---
+++ 1988-MinimizeMaximumPairSumInArray/1988-MinimizeMaximumPairSumInArray.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/3/2026, 1:55:17 PM
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        
+        int max_sum = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n / 2; ++i) {
+            max_sum = max(max_sum, nums[i] + nums[n - 1 - i]);
+        }
+        
+        return max_sum;
+    }
+};
