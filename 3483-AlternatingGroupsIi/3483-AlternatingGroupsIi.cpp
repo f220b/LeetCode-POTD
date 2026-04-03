4bf4b609bb6714b9758810eb56dde375e0d23315
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:51:16 +0530

    Create 3483-AlternatingGroupsIi

---
+++ 3483-AlternatingGroupsIi/3483-AlternatingGroupsIi.cpp
@@ -0,0 +1,23 @@
+// Last updated: 4/3/2026, 1:51:15 PM
+class Solution {
+public:
+    int numberOfAlternatingGroups(vector<int>& colors, int k) {
+        int n = colors.size();
+        int left = 0;
+        int count = 0;
+        int limit = n + (k - 1);
+
+        while (left < n) {
+            int right = left + 1;
+            while (right < limit &&
+                   colors[(right - 1) % n] != colors[right % n]) {
+                right++;
+            }
+            if (right - left >= k) {
+                count += (right - left) - k + 1;
+            }
+            left = right;
+        }
+        return count;
+    }
+};
