56bd5b5231b815f5079293d8c0a41f22cef8d501
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:57:53 +0530

    Create 1093-RecoverATreeFromPreorderTraversal

---
+++ 1093-RecoverATreeFromPreorderTraversal/1093-RecoverATreeFromPreorderTraversal.cpp
@@ -0,0 +1,47 @@
+// Last updated: 4/3/2026, 1:57:52 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* recoverFromPreorder(string traversal) {
+        stack<TreeNode*> st;
+        int i = 0, n = traversal.size();
+
+        while (i < n) {
+            int depth = 0;
+            while (i < n && traversal[i] == '-') {
+                depth++;
+                i++;
+            }
+
+            int num = 0;
+            while (i < n && isdigit(traversal[i])) {
+                num = num * 10 + (traversal[i] - '0');
+                i++;
+            }
+
+            TreeNode* node = new TreeNode(num);
+            while (st.size() > depth) {
+                st.pop();
+            }
+
+            if (!st.empty()) {
+                if (!st.top()->left) st.top()->left = node;
+                else st.top()->right = node;
+            }
+
+            st.push(node);
+        }
+        while (st.size() > 1) st.pop();  // Root remains
+        return st.top();
+    }
+};
