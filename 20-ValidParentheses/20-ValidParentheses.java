11a4358863d5809ac503d59fa9f7783a940be790
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 14:04:25 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,17 @@
+// Last updated: 4/3/2026, 2:04:25 PM
+class Solution {
+    public boolean isValid(String s) {
+      Stack<Character> stack = new Stack<Character>();
+			for (char c : s.toCharArray()) {
+				if (c == '(')
+					stack.push(')');
+				else if (c == '{')
+					stack.push('}');
+				else if (c == '[')
+					stack.push(']');
+				else if (stack.isEmpty() || stack.pop() != c)
+					return false;
+			}
+			return stack.isEmpty();
+    }
+}
