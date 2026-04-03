323d046d6e4ace8cfdfc27d1b1aa55babf9f3385
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 14:00:46 +0530

    Create 263-UglyNumber

---
+++ 263-UglyNumber/263-UglyNumber.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/3/2026, 2:00:46 PM
+class Solution {
+public:
+    bool isUgly(int n) {
+        if (n == 1)
+            return true;
+        if (n == 0)
+            return false;
+        while (n % 5 == 0)
+            n /= 5;
+        while (n % 3 == 0)
+            n /= 3;
+        while (n % 2 == 0)
+            n /= 2;
+        return n == 1;
+    }
+};
