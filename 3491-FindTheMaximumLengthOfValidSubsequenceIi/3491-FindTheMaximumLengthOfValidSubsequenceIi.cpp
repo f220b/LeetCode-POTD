68394dbf6efd7941ae8d13881097b15710e7279f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:51:13 +0530

    Create 3491-FindTheMaximumLengthOfValidSubsequenceIi

---
+++ 3491-FindTheMaximumLengthOfValidSubsequenceIi/3491-FindTheMaximumLengthOfValidSubsequenceIi.cpp
@@ -0,0 +1,17 @@
+// Last updated: 4/3/2026, 1:51:13 PM
+class Solution {
+public:
+    int maximumLength(vector<int>& nums, int k) {
+        vector<vector<int>> dp(k, vector<int>(k, 0));
+        int maxLen = 0;
+
+        for (int num : nums) {
+            for (int j = 0; j < k; ++j) {
+                int remainder = num % k;
+                maxLen = max(maxLen, dp[remainder][j] = dp[j][remainder] + 1);
+            }
+        }
+
+        return maxLen;
+    }
+};
