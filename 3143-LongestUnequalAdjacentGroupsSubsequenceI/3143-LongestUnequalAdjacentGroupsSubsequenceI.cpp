75847e6a475c84b64f6aee0b984330163d13d613
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:52:21 +0530

    Create 3143-LongestUnequalAdjacentGroupsSubsequenceI

---
+++ 3143-LongestUnequalAdjacentGroupsSubsequenceI/3143-LongestUnequalAdjacentGroupsSubsequenceI.cpp
@@ -0,0 +1,13 @@
+// Last updated: 4/3/2026, 1:52:21 PM
+class Solution {
+public:
+    vector<string> getLongestSubsequence(vector<string>& words,
+                                         vector<int>& groups) {
+        vector<string> result;
+        for (int i = 0; i < groups.size(); i++) {
+            if (i == 0 || groups[i] != groups[i - 1])
+                result.push_back(words[i]);
+        }
+        return result;
+    }
+};
