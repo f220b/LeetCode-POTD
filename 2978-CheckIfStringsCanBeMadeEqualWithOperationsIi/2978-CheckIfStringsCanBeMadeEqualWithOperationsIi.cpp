8fd294cff2f84d168d47b5c1009cdfe9bbfd8c89
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:52:30 +0530

    Create 2978-CheckIfStringsCanBeMadeEqualWithOperationsIi

---
+++ 2978-CheckIfStringsCanBeMadeEqualWithOperationsIi/2978-CheckIfStringsCanBeMadeEqualWithOperationsIi.cpp
@@ -0,0 +1,26 @@
+// Last updated: 4/3/2026, 1:52:29 PM
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        int n = s1.length();
+        vector<int> even(26, 0);
+        vector<int> odd(26, 0);
+
+        for (int i = 0; i < n; ++i) {
+            if (i % 2 == 0) {
+                even[s1[i] - 'a']++;
+                even[s2[i] - 'a']--;
+            } else {
+                odd[s1[i] - 'a']++;
+                odd[s2[i] - 'a']--;
+            }
+        }
+
+        for (int i = 0; i < 26; ++i) {
+            if (even[i] != 0 || odd[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
