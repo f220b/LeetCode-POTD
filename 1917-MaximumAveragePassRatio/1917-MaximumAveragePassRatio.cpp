772d6abc1b65f0488eccb4805a6a9f22352701ed
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:55:23 +0530

    Create 1917-MaximumAveragePassRatio

---
+++ 1917-MaximumAveragePassRatio/1917-MaximumAveragePassRatio.cpp
@@ -0,0 +1,32 @@
+// Last updated: 4/3/2026, 1:55:23 PM
+class Solution {
+public:
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        auto gain = [](int pass, int total) {
+            return (double)(pass + 1) / (total + 1) - (double)pass / total;
+        };
+
+        priority_queue<pair<double, pair<int, int>>> pq;
+        for (auto& c : classes) {
+            pq.push({gain(c[0], c[1]), {c[0], c[1]}});
+        }
+
+        while (extraStudents--) {
+            auto [g, p] = pq.top();
+            pq.pop();
+            int pass = p.first, total = p.second;
+            pass++;
+            total++;
+            pq.push({gain(pass, total), {pass, total}});
+        }
+
+        double sum = 0.0;
+        while (!pq.empty()) {
+            auto [g, p] = pq.top();
+            pq.pop();
+            sum += (double)p.first / p.second;
+        }
+
+        return sum / classes.size();
+    }
+};
