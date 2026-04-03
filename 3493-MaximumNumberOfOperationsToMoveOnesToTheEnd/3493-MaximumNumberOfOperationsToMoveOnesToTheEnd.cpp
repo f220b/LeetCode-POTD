4e99f1fbfae74badc4b16a25e72d8c9351930422
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:51:11 +0530

    Create 3493-MaximumNumberOfOperationsToMoveOnesToTheEnd

---
+++ 3493-MaximumNumberOfOperationsToMoveOnesToTheEnd/3493-MaximumNumberOfOperationsToMoveOnesToTheEnd.cpp
@@ -0,0 +1,15 @@
+// Last updated: 4/3/2026, 1:51:11 PM
+class Solution {
+public:
+    int maxOperations(string s) {
+        int ones = 0, res = 0;
+        for (int i = 0; i < s.size(); ++i) {
+            if (s[i] == '1')
+                ones++;
+            else if ((i > 0) && s[i - 1] == '1')
+                res += ones;
+        }
+
+        return res;
+    }
+};
