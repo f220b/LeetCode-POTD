2918ace120e873773ca290960391db1265f85d77
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 14:01:59 +0530

    Create 148-SortList

---
+++ 148-SortList/148-SortList.java
@@ -0,0 +1,63 @@
+// Last updated: 4/3/2026, 2:01:59 PM
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    private static ListNode findMiddle(ListNode head) {
+
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private static ListNode mergeLL(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
+            temp = temp.next;
+        }
+        if (list1 != null)
+            temp.next = list1;
+        else
+            temp.next = list2;
+        return dummy.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+
+        ListNode middle = findMiddle(head);
+        ListNode leftNode = head;
+        ListNode rightNode = middle.next;
+        middle.next = null;
+
+        leftNode = sortList(leftNode);
+        rightNode = sortList(rightNode);
+
+        return mergeLL(leftNode, rightNode);
+    }
+}
