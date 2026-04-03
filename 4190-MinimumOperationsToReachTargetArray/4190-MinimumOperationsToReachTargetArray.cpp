c7010580f3c7cfb89f97e4109d6e04b3132ce4d1
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:48:47 +0530

    Create 4190-MinimumOperationsToReachTargetArray

---
+++ 4190-MinimumOperationsToReachTargetArray/4190-MinimumOperationsToReachTargetArray.cpp
@@ -0,0 +1,12 @@
+// Last updated: 4/3/2026, 1:48:47 PM
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& target) {
+        unordered_set<int> st;
+        for(int i = 0; i < nums.size(); i++) {
+            if(nums[i] != target[i])
+                st.insert(nums[i]);
+        }
+        return st.size();
+    }
+};
