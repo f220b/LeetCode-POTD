20bd814d46f5d13e1e4843007a017ff8f5dab4f5
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 14:00:15 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,18 @@
+// Last updated: 4/3/2026, 2:00:15 PM
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> vec(n, "");
+        for (int i = 0; i < n; i++) {
+            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0)
+                vec[i] = "FizzBuzz";
+            else if ((i + 1) % 3 == 0)
+                vec[i] = "Fizz";
+            else if ((i + 1) % 5 == 0)
+                vec[i] = "Buzz";
+            else
+                vec[i] = to_string(i + 1);
+        }
+        return vec;
+    }
+};
