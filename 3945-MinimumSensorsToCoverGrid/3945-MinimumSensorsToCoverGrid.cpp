9f4a1809cbda41d7c6d0315be5855e5d54d486e7
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:49:26 +0530

    Create 3945-MinimumSensorsToCoverGrid

---
+++ 3945-MinimumSensorsToCoverGrid/3945-MinimumSensorsToCoverGrid.cpp
@@ -0,0 +1,10 @@
+// Last updated: 4/3/2026, 1:49:25 PM
+class Solution {
+public:
+    int minSensors(int n, int m, int k) {
+        int cover = 2 * k + 1;
+        int rows = (n + cover - 1) / cover;
+        int cols = (m + cover - 1) / cover;
+        return rows * cols;
+    }
+};
