6e6b70dc359b64f33dc046fc75d2864405007bd9
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:52:10 +0530

    Create 3215-MatrixSimilarityAfterCyclicShifts

---
+++ 3215-MatrixSimilarityAfterCyclicShifts/3215-MatrixSimilarityAfterCyclicShifts.cpp
@@ -0,0 +1,16 @@
+// Last updated: 4/3/2026, 1:52:10 PM
+class Solution {
+public:
+    bool areSimilar(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat[0].size();
+        k = k % n;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] != mat[i][(j - k + n) % n])
+                    return false;
+            }
+        }
+        return true;
+    }
+};
