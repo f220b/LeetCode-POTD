4c7d28354bc1702d7a204d0ae7384b31e4043e7e
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 14:03:26 +0530

    Create 64-MinimumPathSum

---
+++ 64-MinimumPathSum/64-MinimumPathSum.cpp
@@ -0,0 +1,24 @@
+// Last updated: 4/3/2026, 2:03:26 PM
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = grid[0][0];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                int up = INT_MAX, left = INT_MAX;
+                if (i - 1 >= 0)
+                    up = grid[i][j] + dp[i - 1][j];
+                if (j - 1 >= 0)
+                    left = grid[i][j] + dp[i][j - 1];
+
+                dp[i][j] = min(up, left);
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
