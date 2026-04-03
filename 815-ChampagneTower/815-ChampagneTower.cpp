9365c867a6ca9253abcd0c9876bd4f390b1b3d4c
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:58:48 +0530

    Create 815-ChampagneTower

---
+++ 815-ChampagneTower/815-ChampagneTower.cpp
@@ -0,0 +1,22 @@
+// Last updated: 4/3/2026, 1:58:48 PM
+class Solution {
+public:
+    double champagneTower(int poured, int query_row, int query_glass) {
+        double tower[101][101] = {0.0};
+
+        tower[0][0] = (double)poured;
+
+        for (int i = 0; i <= query_row; ++i) {
+            for (int j = 0; j <= i; ++j) {
+                if (tower[i][j] > 1.0) {
+                    double excess = (tower[i][j] - 1.0) / 2.0;
+                    tower[i + 1][j] += excess;
+                    tower[i + 1][j + 1] += excess;
+                    tower[i][j] = 1.0;
+                }
+            }
+        }
+
+        return tower[query_row][query_glass];
+    }
+};
