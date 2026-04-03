4d3b37fdd5b5e0b4104f06124a295808ba2fcdfb
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-04-03 13:50:57 +0530

    Create 3611-ConstructTheMinimumBitwiseArrayIi

---
+++ 3611-ConstructTheMinimumBitwiseArrayIi/3611-ConstructTheMinimumBitwiseArrayIi.py
@@ -0,0 +1,17 @@
+# Last updated: 4/3/2026, 1:50:57 PM
+class Solution:
+    def minBitwiseArray(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        ans = [-1]*n
+
+        for i in range(0, n):
+            if nums[i] % 2 == 0:
+                continue
+            target = nums[i]
+
+            for bit in range(32):
+                if not (target & (1 << bit)):
+                    res = target ^ (1 << (bit - 1))
+                    ans[i] = res
+                    break
+        return ans
