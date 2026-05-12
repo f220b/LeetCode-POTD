3a33d313bef289e816e123270f40e36437107a0f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-05-12 13:37:39 +0530

    Create 2553-SeparatetheDigitsinanArray

---
+++ 2553-SeparatetheDigitsinanArray/2553-SeparatetheDigitsinanArray.cpp
@@ -0,0 +1,13 @@
+// Last updated: 5/12/2026, 1:37:38 PM
+1class Solution {
+2public:
+3    vector<int> separateDigits(vector<int>& nums) {
+4        vector<int> result;
+5        for(int num : nums) {
+6            string str = to_string(num);
+7            for(char ch : str)
+8                result.push_back(ch - '0');
+9        }
+10        return result;
+11    }
+12};
