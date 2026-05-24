aad6399307edc275b364719d2f83dfad273d7c81
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-05-24 20:42:30 +0530

    Create 4300-MinimumOperationsToMakeArrayModuloAlternatingI

---
+++ 4300-MinimumOperationsToMakeArrayModuloAlternatingI/4300-MinimumOperationsToMakeArrayModuloAlternatingI.cpp
@@ -0,0 +1,28 @@
+// Last updated: 24/5/2026, 8:42:27 pm
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int n = nums.size();
+        int minOps = INT_MAX;
+
+        for (int x = 0; x < k; x++) {
+            for (int y = 0; y < k; y++) {
+                if (x == y)
+                    continue;
+
+                int ops = 0;
+                for (int i = 0; i < n; i++) {
+                    int target = x;
+                    if (i & 1)
+                        target = y;
+
+                    int curr = nums[i] % k;
+                    int diff = abs(curr - target);
+                    ops += min(diff, k - diff);
+                }
+                minOps = min(minOps, ops);
+            }
+        }
+        return minOps;
+    }
+};
