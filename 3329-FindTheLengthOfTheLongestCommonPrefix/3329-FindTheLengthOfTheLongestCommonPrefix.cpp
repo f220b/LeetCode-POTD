bf80fbea1427718c54748d3a45137001b861440c
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-05-30 22:18:29 +0530

    Create 3329-FindTheLengthOfTheLongestCommonPrefix

---
+++ 3329-FindTheLengthOfTheLongestCommonPrefix/3329-FindTheLengthOfTheLongestCommonPrefix.cpp
@@ -0,0 +1,30 @@
+// Last updated: 30/5/2026, 10:18:29 pm
+class Solution {
+public:
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        unordered_set<string> st;
+        // O(n * log(k))
+        for(int num : arr1) {
+            string str = to_string(num);
+            string prefix = "";
+            for(char ch : str) {
+                prefix += ch;
+                st.insert(prefix);
+            }
+        }
+        // O(n * log(k))
+        int maxLen = 0;
+        for(int num : arr2) {
+            string str = to_string(num);
+            string prefix = "";
+            int size = 0;
+            for(char ch : str) {
+                prefix += ch;
+                size++;
+                if(st.count(prefix)) 
+                    maxLen = max(maxLen, size);
+            }
+        }
+        return maxLen;
+    }
+};
