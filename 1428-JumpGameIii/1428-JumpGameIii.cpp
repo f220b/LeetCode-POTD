82c4de5cd7230afaf0baa15d4961bb1387a7ea39
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-05-30 22:22:50 +0530

    Create 1428-JumpGameIii

---
+++ 1428-JumpGameIii/1428-JumpGameIii.cpp
@@ -0,0 +1,25 @@
+// Last updated: 30/5/2026, 10:22:50 pm
+class Solution {
+public:
+    int n;
+    vector<int> visited;
+    bool helper(vector<int>& arr, int i) {
+        if (arr[i] == 0)
+            return true;
+
+        visited[i] = 1;
+        bool case1 = false, case2 = false;
+        if (i - arr[i] >= 0 && !visited[i - arr[i]])
+            case1 = helper(arr, i - arr[i]);
+        if (i + arr[i] < n && !visited[i + arr[i]])
+            case2 = helper(arr, i + arr[i]);
+
+        return case1 || case2;
+    }
+    bool canReach(vector<int>& arr, int start) {
+        n = arr.size();
+        visited.resize(n, 0);
+        visited[start] = 1;
+        return helper(arr, start);
+    }
+};
