2022d4a034c584700e4d7f5748e08cf951dd1701
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-07-03 10:26:07 +0530

    Create 3939-ProcessStringWithSpecialOperationsIi

---
+++ 3939-ProcessStringWithSpecialOperationsIi/3939-ProcessStringWithSpecialOperationsIi.cpp
@@ -0,0 +1,40 @@
+// Last updated: 7/3/2026, 10:26:07 AM
+class Solution {
+    typedef long long ll;
+public:
+    char processStr(string s, ll k) {
+        int n = s.size();
+        ll len = 0;
+
+        for (auto& ch : s) {
+            if (ch == '*')
+                len = max(len - 1, 0LL);
+            else if (ch == '#')
+                len *= 2;
+            else if (ch != '%')
+                len++;
+        }
+
+        if (k >= len) return '.';
+
+        for (int i = n - 1;; i--) {
+            switch (s[i]) {
+            case '*':
+                len++;
+                break;
+            case '#':
+                if (k >= len / 2)
+                    k -= len / 2;
+                len /= 2;
+                break;
+            case '%':
+                k = len - 1 - k;
+                break;
+            default:
+                if (len == k + 1)
+                    return s[i];
+                len--;
+            }
+        }
+    }
+};
