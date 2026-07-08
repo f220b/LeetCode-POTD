5a3e56a33f13de3c7151193aefef85eacd24922f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-07-08 10:10:51 +0530

    Create 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI

---
+++ 4135-ConcatenateNonZeroDigitsAndMultiplyBySumI/4135-ConcatenateNonZeroDigitsAndMultiplyBySumI.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/8/2026, 10:10:50 AM
+class Solution {
+public:
+    using ll = long long;
+    long long sumAndMultiply(int n) {
+        int x = 0, sum = 0;
+        int mul = 1;
+
+        while (n) {
+            int d = n % 10;
+            if (d) {
+                x = x + (d * mul);
+                mul *= 10;
+            }
+            sum += d;
+            n /= 10;
+        }
+        return (ll)x * sum;
+    }
+};
