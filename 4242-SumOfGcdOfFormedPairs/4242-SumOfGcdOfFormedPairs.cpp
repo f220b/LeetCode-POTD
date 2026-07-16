d36a5c9e8fe61fe31ecd5a380fec7c2605bb890f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-07-16 20:35:20 +0530

    Create 4242-SumOfGcdOfFormedPairs

---
+++ 4242-SumOfGcdOfFormedPairs/4242-SumOfGcdOfFormedPairs.cpp
@@ -0,0 +1,25 @@
+// Last updated: 7/16/2026, 8:35:20 PM
+class Solution {
+public:
+    using ll = long long;
+    using vi = vector<int>;
+    long long gcdSum(vector<int>& nums) {
+        int n = nums.size();
+        vi prefixGcd(n);
+        int mx = INT_MIN;
+        for (int i = 0; i < n; i++) {
+            mx = max(nums[i], mx);
+            prefixGcd[i] = gcd(mx, nums[i]);
+        }
+        sort(prefixGcd.rbegin(), prefixGcd.rend());
+
+        int i = 0, j = n - 1;
+        ll sum = 0;
+        while (i < j) {
+            sum += (ll)gcd(prefixGcd[i], prefixGcd[j]);
+            i++;
+            j--;
+        }
+        return sum;
+    }
+};
