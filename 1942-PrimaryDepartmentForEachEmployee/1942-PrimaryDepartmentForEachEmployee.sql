eedc51326e869f18f10a2edfb1d4cb6ba5909dfa
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-08-06 13:13:18 +0530

    Create 1942-PrimaryDepartmentForEachEmployee

---
+++ 1942-PrimaryDepartmentForEachEmployee/1942-PrimaryDepartmentForEachEmployee.sql
@@ -0,0 +1,15 @@
+-- Last updated: 8/6/2026, 1:13:18 PM
+# Write your MySQL query statement below
+SELECT 
+    employee_id, 
+    department_id
+FROM Employee 
+WHERE primary_flag = 'Y'
+UNION
+SELECT 
+    employee_id, 
+    department_id
+FROM Employee 
+GROUP BY employee_id
+HAVING COUNT(employee_id) = 1
+ORDER BY employee_id;
