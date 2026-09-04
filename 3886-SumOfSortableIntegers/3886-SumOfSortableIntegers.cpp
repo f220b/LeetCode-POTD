5d85e8ba64b95581ad7e1845e0e084592d34b479
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:02:45 +0530

    Create 3886-SumOfSortableIntegers

---
+++ 3886-SumOfSortableIntegers/3886-SumOfSortableIntegers.cpp
@@ -0,0 +1,40 @@
+// Last updated: 9/5/2026, 12:02:45 AM
+class Solution {
+public:
+    int sortableIntegers(vector<int>& nums) {
+        int n = nums.size(), ans = 0;
+        vector<int> sorted_nums = nums;
+        sort(sorted_nums.begin(), sorted_nums.end());
+
+        for (int k = 1; k <= n; k++) {
+            if (n % k != 0)
+                continue;
+
+            bool sortable = true;
+            for (int i = 0; i < n && sortable; i += k) {
+                vector<int> chunk(nums.begin() + i, nums.begin() + i + k);
+                vector<int> target(sorted_nums.begin() + i,
+                                   sorted_nums.begin() + i + k);
+
+                vector<int> sorted_chunk = chunk;
+                sort(sorted_chunk.begin(), sorted_chunk.end());
+                if (sorted_chunk != target) {
+                    sortable = false;
+                    break;
+                }
+
+                vector<int> doubled = target;
+                doubled.insert(doubled.end(), target.begin(), target.end());
+                auto it = search(doubled.begin(), doubled.end(), chunk.begin(),
+                                 chunk.end());
+                if (it == doubled.end()) {
+                    sortable = false;
+                    break;
+                }
+            }
+            if (sortable)
+                ans += k;
+        }
+        return ans;
+    }
+};
