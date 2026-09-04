5298253fdbf61759383af0ca825cb629a863bd1f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:22:27 +0530

    Create 2197-ReplaceNonCoprimeNumbersInArray

---
+++ 2197-ReplaceNonCoprimeNumbersInArray/2197-ReplaceNonCoprimeNumbersInArray.cpp
@@ -0,0 +1,26 @@
+// Last updated: 9/5/2026, 12:22:27 AM
+#include <numeric>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> replaceNonCoprimes(vector<int>& nums) {
+        vector<int> st;
+        for (int x : nums) {
+            st.push_back(x);
+            while (st.size() > 1) {
+                int a = st[st.size() - 2];
+                int b = st[st.size() - 1];
+                int g = gcd(a, b);
+                if (g == 1)
+                    break;
+                long long l = 1LL * a / g * b;
+                st.pop_back();
+                st.pop_back();
+                st.push_back((int)l);
+            }
+        }
+        return st;
+    }
+};
