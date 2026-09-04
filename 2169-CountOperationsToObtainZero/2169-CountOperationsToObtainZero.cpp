cedecb596fa3bb3c2409885b998443e3a50795b3
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:22:36 +0530

    Create 2169-CountOperationsToObtainZero

---
+++ 2169-CountOperationsToObtainZero/2169-CountOperationsToObtainZero.cpp
@@ -0,0 +1,15 @@
+// Last updated: 9/5/2026, 12:22:35 AM
+class Solution {
+public:
+    int countOperations(int num1, int num2) {
+        int count = 0;
+        while(num1 != 0 && num2 != 0) {
+            if(num1 >= num2)
+                num1 -= num2;
+            else
+                num2 -= num1;
+            count++;
+        }
+        return count;
+    }
+};
