36f5a1f8562fb33bfa9a096a945f790ebfebf1ca
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:24:47 +0530

    Create 2029-StoneGameIx

---
+++ 2029-StoneGameIx/2029-StoneGameIx.py
@@ -0,0 +1,12 @@
+# Last updated: 9/5/2026, 12:24:47 AM
+class Solution:
+    def stoneGameIX(self, stones: List[int]) -> bool:
+        f = [0, 0, 0]
+
+        for s in stones:
+            f[s % 3] += 1
+
+        if ~f[0] & 1:
+            return min(f[1], f[2]) >= 1
+
+        return abs(f[1] - f[2]) >= 3
