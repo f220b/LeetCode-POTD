fddabbe2781aff5f8793ace5efd6b79882403bc2
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:09:56 +0530

    Create 3487-MaximumUniqueSubarraySumAfterDeletion

---
+++ 3487-MaximumUniqueSubarraySumAfterDeletion/3487-MaximumUniqueSubarraySumAfterDeletion.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/5/2026, 12:09:55 AM
+class Solution {
+public:
+    int maxSum(vector<int>& nums) {
+        bool allNegative = true;
+        int maxVal = INT_MIN;
+
+        for (int n : nums) {
+            if (n >= 0)
+                allNegative = false;
+            if (n > maxVal)
+                maxVal = n;
+        }
+        if (allNegative)
+            return maxVal;
+
+        unordered_set<int> unique(nums.begin(), nums.end());
+
+        int sum = 0;
+        for (int val : unique) {
+            if (val > 0)
+                sum += val;
+        }
+
+        return sum;
+    }
+};
