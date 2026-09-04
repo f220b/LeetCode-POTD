0e89a1991addec083a8bdee7f70bf3edde03ea0f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:36:53 +0530

    Create 874-WalkingRobotSimulation

---
+++ 874-WalkingRobotSimulation/874-WalkingRobotSimulation.cpp
@@ -0,0 +1,37 @@
+// Last updated: 9/5/2026, 12:36:53 AM
+class Solution {
+public:
+    int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
+        unordered_set<string> obs;
+        for (const auto& it : obstacles) {
+            obs.insert(to_string(it[0]) + "," + to_string(it[1]));
+        }
+
+        vector<int> dx = {0, 1, 0, -1};
+        vector<int> dy = {1, 0, -1, 0};
+        int x = 0, y = 0, dir = 0;
+        int maxDist = 0;
+
+        for (int cmd : commands) {
+            if (cmd == -2)
+                dir = (dir + 3) % 4;
+            else if (cmd == -1)
+                dir = (dir + 1) % 4;
+            else {
+                for (int i = 0; i < cmd; i++) {
+                    int newX = x + dx[dir];
+                    int newY = y + dy[dir];
+                    if (obs.find(to_string(newX) + "," + to_string(newY)) ==
+                        obs.end()) {
+                        x = newX;
+                        y = newY;
+                        maxDist = max(maxDist, x * x + y * y);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        return maxDist;
+    }
+};
