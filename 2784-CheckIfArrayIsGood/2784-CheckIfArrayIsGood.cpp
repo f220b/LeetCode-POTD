7bac2f57870c66c68dd447e5b54ec80a6adeb8f8
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:17:51 +0530

    Create 2784-CheckIfArrayIsGood

---
+++ 2784-CheckIfArrayIsGood/2784-CheckIfArrayIsGood.cpp
@@ -0,0 +1,22 @@
+// Last updated: 9/5/2026, 12:17:51 AM
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int mx = *max_element(nums.begin(), nums.end());
+        if(nums.size() != mx + 1)
+            return false;
+
+        vector<int> freq(mx + 1, 0);
+        for(int x : nums) {
+            if(x < 1 || x > mx)
+                return false;
+            freq[x]++;
+        }
+
+        for(int i = 1; i < mx; i++) {
+            if(freq[i] != 1)
+                return false;
+        }
+        return freq[mx] == 2;
+    }
+};
