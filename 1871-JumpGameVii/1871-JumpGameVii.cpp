a221a5c7f109ef076eedc781b760f35baec7d5dc
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:26:27 +0530

    Create 1871-JumpGameVii

---
+++ 1871-JumpGameVii/1871-JumpGameVii.cpp
@@ -0,0 +1,31 @@
+// Last updated: 9/5/2026, 12:26:27 AM
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        int n = s.length();
+        if (s[n - 1] != '0')
+            return false;
+        vector<bool> dp(n, false);
+        dp[n - 1] = true;
+
+        int active_jumps = 0;
+        int right = n - 1;
+        int left = n - 1;
+
+        for (int idx = n - 2; idx >= 0; idx--) {
+            while (left >= idx + minJump) {
+                if (dp[left]) 
+                    active_jumps++;
+                left--;
+            }
+            while (right > idx + maxJump) {
+                if (dp[right]) 
+                    active_jumps--;
+                right--;
+            }
+            if (s[idx] == '0' && active_jumps > 0) 
+                dp[idx] = true;
+        }
+        return dp[0];
+    }
+};
