7146b05ec045f8c88d692ceab46be249547a3ec3
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:05:22 +0530

    Create 3970-ShortestPathWithAtMostKConsecutiveIdenticalCharacters

---
+++ 3970-ShortestPathWithAtMostKConsecutiveIdenticalCharacters/3970-ShortestPathWithAtMostKConsecutiveIdenticalCharacters.cpp
@@ -0,0 +1,42 @@
+// Last updated: 9/5/2026, 12:05:21 AM
+class Solution {
+public:
+    using ll = long long;
+    using tlii = tuple<ll, int, int>;
+
+    int shortestPath(int n, vector<vector<int>>& edges, string labels, int k) {
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto edge : edges) 
+            adj[edge[0]].push_back({edge[1], edge[2]});
+
+        priority_queue<tlii, vector<tlii>, greater<tlii>> pq;
+        vector<vector<ll>> dist(n, vector<ll>(k + 1, 1e18));
+        dist[0][1] = 0;
+        pq.push({0, 0, 1});
+
+        while (!pq.empty()) {
+            auto [d, u, cnt] = pq.top();
+            pq.pop();
+
+            if (d > dist[u][cnt])
+                continue;
+            if (u == n - 1)
+                return d;
+
+            for (auto edge : adj[u]) {
+                int v = edge.first;
+                int w = edge.second;
+
+                int next_cnt = (labels[u] == labels[v]) ? cnt + 1 : 1;
+                if (next_cnt <= k) {
+                    if (d + w < dist[v][next_cnt]) {
+                        dist[v][next_cnt] = d + w;
+                        pq.push({dist[v][next_cnt], v, next_cnt});
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+};
