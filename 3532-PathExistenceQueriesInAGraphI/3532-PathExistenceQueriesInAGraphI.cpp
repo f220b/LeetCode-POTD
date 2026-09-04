f23a3917bb472b07d359e6f9f2e9f6bb416ffb1c
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:09:03 +0530

    Create 3532-PathExistenceQueriesInAGraphI

---
+++ 3532-PathExistenceQueriesInAGraphI/3532-PathExistenceQueriesInAGraphI.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/5/2026, 12:09:03 AM
+#define pb push_back
+class Solution {
+public:
+    using vi = vector<int>;
+    vector<bool> pathExistenceQueries(int n, vi& nums, int maxDiff,
+                                      vector<vi>& queries) {
+        vector<int> breaks;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff)
+                continue;
+            breaks.pb(i - 1);
+        }
+
+        vector<bool> ans;
+        for (auto x : queries) {
+            int u = x[0], v = x[1];
+            auto itr1 = lower_bound(breaks.begin(), breaks.end(), u);
+            auto itr2 = lower_bound(breaks.begin(), breaks.end(), v);
+            if (itr1 == itr2)
+                ans.pb(true);
+            else
+                ans.pb(false);
+        }
+        return ans;
+    }
+};
