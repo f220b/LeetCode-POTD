be74d4449d1ad7974b6da832b215fd86265126b4
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:13:37 +0530

    Create 3296-MinimumNumberOfSecondsToMakeMountainHeightZero

---
+++ 3296-MinimumNumberOfSecondsToMakeMountainHeightZero/3296-MinimumNumberOfSecondsToMakeMountainHeightZero.py
@@ -0,0 +1,25 @@
+# Last updated: 9/5/2026, 12:13:37 AM
+import heapq
+
+
+class Solution:
+    def minNumberOfSeconds(self, h, t):
+        pq = []
+
+        for i in range(len(t)):
+            heapq.heappush(pq, (t[i], i, 1))
+
+        res = 0
+
+        while h > 0:
+            tm, idx, x = heapq.heappop(pq)
+
+            res = tm
+            h -= 1
+
+            if h > 0:
+                nx = x + 1
+                nt = t[idx] * (nx * (nx + 1) // 2)
+                heapq.heappush(pq, (nt, idx, nx))
+
+        return res
