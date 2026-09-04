159f5028dad12a85a4540b40163a749940632056
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:07:14 +0530

    Create 3654-MinimumSumAfterDivisibleSumDeletions

---
+++ 3654-MinimumSumAfterDivisibleSumDeletions/3654-MinimumSumAfterDivisibleSumDeletions.cpp
@@ -0,0 +1,27 @@
+// Last updated: 9/5/2026, 12:07:14 AM
+class Solution {
+public:
+    long long minArraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<long long> prefixSum(n + 1, 0);
+        for (int i = 1; i <= n; i++)
+            prefixSum[i] = prefixSum[i - 1] + nums[i - 1];
+
+        vector<long long> dp(n + 1, 0);
+        vector<long long> minDpForResidue(k, LLONG_MAX);
+        minDpForResidue[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = dp[i - 1] + nums[i - 1];
+
+            int residue = prefixSum[i] % k;
+            if (minDpForResidue[residue] != LLONG_MAX) {
+                if (minDpForResidue[residue] < dp[i])
+                    dp[i] = minDpForResidue[residue];
+            }
+            if (dp[i] < minDpForResidue[residue])
+                minDpForResidue[residue] = dp[i];
+        }
+        return dp[n];
+    }
+};
