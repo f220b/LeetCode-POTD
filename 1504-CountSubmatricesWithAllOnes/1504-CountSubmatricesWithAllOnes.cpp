a55ed7e428fadc51d3a3d6c1f2c7421cd1182716
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:30:07 +0530

    Create 1504-CountSubmatricesWithAllOnes

---
+++ 1504-CountSubmatricesWithAllOnes/1504-CountSubmatricesWithAllOnes.cpp
@@ -0,0 +1,38 @@
+// Last updated: 9/5/2026, 12:30:07 AM
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> heights(n, 0);
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1)
+                    heights[j]++;
+                else
+                    heights[j] = 0;
+            }
+
+            stack<int> st;
+            vector<int> dp(n, 0);
+
+            for (int j = 0; j < n; j++) {
+                while (!st.empty() && heights[st.top()] >= heights[j])
+                    st.pop();
+
+                if (!st.empty()) {
+                    int prev = st.top();
+                    dp[j] = dp[prev] + heights[j] * (j - prev);
+                } else {
+                    dp[j] = heights[j] * (j + 1);
+                }
+
+                st.push(j);
+                ans += dp[j];
+            }
+        }
+
+        return ans;
+    }
+};
