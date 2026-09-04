f11f30f143c8a85e6244cc222246b5089a8053be
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:12:53 +0530

    Create 3336-FindTheNumberOfSubsequencesWithEqualGcd

---
+++ 3336-FindTheNumberOfSubsequencesWithEqualGcd/3336-FindTheNumberOfSubsequencesWithEqualGcd.cpp
@@ -0,0 +1,31 @@
+// Last updated: 9/5/2026, 12:12:52 AM
+class Solution {
+public:
+    const int MOD = 1e9 + 7;
+    using vi = vector<int>;
+    int n;
+    int dp[201][201][201];
+
+    int helper(int i, int g1, int g2, vi& nums) {
+        if (i == n)
+            return (g1 != 0 && g1 == g2);
+
+        int ans = 0;
+        if (dp[i][g1][g2] != -1)
+            return dp[i][g1][g2];
+
+        ans = helper(i + 1, g1, g2, nums);
+        int ng1 = (g1 == 0) ? nums[i] : gcd(g1, nums[i]);
+        ans = (ans + helper(i + 1, ng1, g2, nums)) % MOD;
+
+        int ng2 = (g2 == 0) ? nums[i] : gcd(g2, nums[i]);
+        ans = (ans + helper(i + 1, g1, ng2, nums)) % MOD;
+
+        return dp[i][g1][g2] = ans;
+    }
+    int subsequencePairCount(vector<int>& nums) {
+        n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        return helper(0, 0, 0, nums);
+    }
+};
