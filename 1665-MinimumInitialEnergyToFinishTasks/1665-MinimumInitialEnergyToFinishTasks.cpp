c971978d7c3f5559cbf7d092eefebe2f19906c4f
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-05 00:28:46 +0530

    Create 1665-MinimumInitialEnergyToFinishTasks

---
+++ 1665-MinimumInitialEnergyToFinishTasks/1665-MinimumInitialEnergyToFinishTasks.cpp
@@ -0,0 +1,34 @@
+// Last updated: 9/5/2026, 12:28:45 AM
+class Solution {
+public:
+    bool static compare(const vector<int>& a, const vector<int>& b) {
+        return (a[1] - a[0]) > (b[1] - b[0]);
+    }
+    bool isValid(int nrg, vector<vector<int>>& tasks) {
+        for (auto task : tasks) {
+            if (task[1] > nrg)
+                return false;
+            nrg -= task[0];
+        }
+        return true;
+    }
+    int minimumEffort(vector<vector<int>>& tasks) {
+        sort(tasks.begin(), tasks.end(), compare);
+        int low = 0, high = 0;
+        for (auto itr : tasks) {
+            low = max(low, itr[1]);
+            high += itr[1];
+        }
+        int ans = high;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (isValid(mid, tasks)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
