ca142fad57d00cc0ffacb8a0b42954869da29878
Parth Sarthi <100792474+f220b@users.noreply.github.com>
2026-09-22 23:27:51 +0530

    Create 1401-CircleAndRectangleOverlapping

---
+++ 1401-CircleAndRectangleOverlapping/1401-CircleAndRectangleOverlapping.py
@@ -0,0 +1,7 @@
+# Last updated: 9/22/2026, 11:27:51 PM
+class Solution:
+    def checkOverlap(self, r: int, cx: int, cy: int, x1: int, y1: int, x2: int, y2: int) -> bool:
+        x = max(x1, min(cx, x2)) - cx
+        y = max(y1, min(cy, y2)) - cy
+
+        return x * x + y * y <= r * r
